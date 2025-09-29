a45888133bd84a1abf27989a66e74b1829f3f955
npaccoud <nathpacc@gmail.com>
2025-09-29 12:37:26 +0200

    Adding timer

--- cmd/lem-in/lem-in.go
+++ cmd/lem-in/lem-in.go
@@ -6,13 +6,15 @@ import (
 	"lem-in/datas"
 	"os"
 	"strings"
+	"time"
 )
 
 // main parses arguments, loads data, checks for errors, builds the colony, and prints the solution.
 func main() {
 	// Check for correct usage
+	start := time.Now()
 	if len(os.Args) != 2 && len(os.Args) != 3 {
-		fmt.Println("Error : Usage is './lem-in filename'")
+		fmt.Println("Error : Usage is './lem-in filename' or './lem-in filename | ./visualizer")
 		return
 	}
 	filename := os.Args[1]
@@ -32,7 +34,15 @@ func main() {
 	rooms := colony.CreatRooms(filedatas)
 	colony.CreatColony(filedatas, rooms)
 	// Find and print the best solution
+	durationColony := time.Since(start)
+	startAlgo := time.Now()
 	bestset := colony.Resolve(filedatas.NbAnts, rooms)
 	fmt.Println(instructions + "\n")
 	colony.PrintResolve(filedatas.NbAnts, bestset)
+	durationAll := time.Since(start)
+	durationAlgo := time.Since(startAlgo)
+	fmt.Println("--------------------")
+	fmt.Printf("Colony constructed in %s\n", durationColony)
+	fmt.Printf("Algo resolution done in %s\n", durationAlgo)
+	fmt.Printf("Total resolution done in %s\n", durationAll)
 }
